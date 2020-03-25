dab45e22a47365adfe8fbe6cd0b15d023ed0c845
Veronika <veronika.jefimova03@gmai.com>
2020-03-25 12:00:56 +0200

    Use String Interpolation

--- Program.cs
+++ Program.cs
@@ -13,7 +13,10 @@ namespace TARpv19_Jefimova
             Console.WriteLine("Please, enter your name:");
             string name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name + "!");
+            //Console.WriteLine("Hello " + name + "!");
+            //string interpolation
+
+            Console.WriteLine($"Hello, {name}");
 
             Console.ReadLine();
         }
